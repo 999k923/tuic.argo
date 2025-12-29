1aee14a56ffd65d2295cbdff65a520960de2a117
999k923 <419879397@qq.com>
2025-12-29 13:52:34 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -275,6 +275,7 @@ cat > "$CONFIG_PATH" <<EOF
       "type": "vless",
       "tag": "vless-anytls",
       "listen": "::",
+      "dual_stack": true,
       "listen_port": ${ANYTLS_PORT},
       "users": [
         {
