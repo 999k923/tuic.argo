7fc0b2c47544e1d0474820473be4b3b7592e5518
999k923 <419879397@qq.com>
2026-01-01 10:53:49 +0800

    Update x.sh

--- x.sh
+++ x.sh
@@ -119,7 +119,7 @@ REALITY_IN_PORT=44312
 cat >/etc/xray/config.json <<EOF
 {
   "log": {
-  "loglevel": "debug",
+  "loglevel": "warning",
   "access": "/var/log/xray/access.log",
   "error": "/var/log/xray/error.log"
 },
